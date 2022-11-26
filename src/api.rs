36adb52612c3441bcff916153ec29a60e641be2f
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-11-26 12:27:12 +0300

    impl get_acct function

--- src/api.rs
+++ src/api.rs
@@ -50,8 +50,114 @@ pub async fn api_helpers() -> Result<(), failure::Error>  {
     Ok(())
 }
 
+// pub async fn place_market_order(
+//     api: &ftx::rest::Rest,
+//     market_name: &str,
+//     order_side: ftx::rest::Side,
+//     order_size: rust_decimal::Decimal) -> bool {
+//     let order = api.request(ftx::rest::PlaceOrder {
+//         market: std::string::String::from(market_name),
+//         side: order_side,
+//         price: None,
+//         r#type: ftx::rest::OrderType::Market,
+//         size: order_size,
+//         reduce_only: false,
+//         ioc: false,
+//         post_only: false,
+//         client_id: None,
+//         reject_on_price_band: false,
+//     }).await;
+
+//     let order_success;
+//     match order {
+//         Err(e) => {
+//             log::error!("Unable to place order, Err: {:?}", e);
+//             order_success = false;
+//         }
+//         Ok(o) => {
+//             log::info!("Order placed successfully: {:?}", o);
+//             order_success = true;
+//         }
+//     };
+
+//     return order_success;
+// }
+
 // async fn get_account_info(info: String) -> Result<()> {
 
 //     Ok(())
 // }
 
+// pub struct TradingPair {
+//     symbol: String,
+//     base_asset: String,
+//     quote_asset: String,
+//     step: f64,
+//     initialized: bool,
+//     asks: Vec<Asks>,
+//     bids: Vec<Bids>,
+//     timestamp: SystemTime,
+//   }
+  
+//   impl fmt::Display for TradingPair {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//       write!(f, "<TradingPair {}/{}>", self.base_asset, self.quote_asset)
+//     }
+//   }
+  
+//   impl TradingPair {
+//     // Constructor
+//     pub fn new(symbol: String, base_asset: String, quote_asset: String, step: f64) -> TradingPair {
+//       TradingPair {
+//         symbol,
+//         base_asset,
+//         quote_asset,
+//         step,
+//         initialized: false,
+//         asks: Vec::new(),
+//         bids: Vec::new(),
+//         timestamp: SystemTime::now(),
+//       }
+//     }
+//     // Setters
+//     pub fn update(&mut self, timestamp: SystemTime, asks: Vec<Asks>, bids: Vec<Bids>) {
+//       self.asks = asks;
+//       self.bids = bids;
+//       self.timestamp = timestamp;
+//       self.initialized = true;
+//     }
+//     // Getters
+//     pub fn get_symbol(&self) -> String {
+//       self.symbol.to_string()
+//     }
+//     pub fn get_step(&self) -> f64 {
+//       self.step
+//     }
+//     pub fn get_base_asset(&self) -> String {
+//       self.base_asset.to_string()
+//     }
+//     pub fn get_quote_asset(&self) -> String {
+//       self.quote_asset.to_string()
+//     }
+//     // Utilities
+//     pub fn has_asset(&self, asset: String) -> bool {
+//       if (asset == self.quote_asset) || (asset == self.base_asset) {
+//         return true;
+//       }
+//       false
+//     }
+//     pub fn get_the_other(&self, asset: String) -> String {
+//       if asset == self.quote_asset {
+//         self.base_asset.to_string()
+//       } else if asset == self.base_asset {
+//         self.quote_asset.to_string()
+//       } else {
+//         "".to_string()
+//       }
+//     }
+  
+//     pub fn text(&self) -> String {
+//       format!("{}/{}", self.base_asset, self.quote_asset)
+//     }
+//   }
+  
