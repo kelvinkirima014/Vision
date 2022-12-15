d88c998cbea409ea3ca2a0c7ec36f4ac443b1f81
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-12-15 04:54:01 +0300

    uncomment main api

--- src/api.rs
+++ src/api.rs
@@ -8,47 +8,47 @@ use kucoin_rs::kucoin::model::websocket::{KucoinWebsocketMsg, WSType, WSTopic};
 use dotenv::dotenv;
 use std::env;
 
-// #[tokio::main]
-// pub async fn init_kucoin_api() -> Result<(), anyhow::Error>  {
-//     dotenv().ok();
+#[tokio::main]
+pub async fn init_kucoin_api() -> Result<(), Box<dyn Error>>  {
+    dotenv().ok();
     
-//     let api_key = env::var("KUCOIN_API_KEY").expect("missing API key");
-//     let secret_key = env::var("KUCOIN_SECRET_KEY").expect("wrong secret key");
-//     let passphrase = env::var("KUCOIN_PASSPHRASE").expect("Wrong passphrase");
+    let api_key = env::var("KUCOIN_API_KEY").expect("missing API key");
+    let secret_key = env::var("KUCOIN_SECRET_KEY").expect("wrong secret key");
+    let passphrase = env::var("KUCOIN_PASSPHRASE").expect("Wrong passphrase");
 
-//     //generate a new Credentials struct w/ the necessary keys
-//     let credentials = Credentials::new(
-//         &api_key,
-//         &secret_key,
-//         &passphrase,
-//     );
+    //generate a new Credentials struct w/ the necessary keys
+    let credentials = Credentials::new(
+        &api_key,
+        &secret_key,
+        &passphrase,
+    );
 
-//     // Initialize the Kucoin API struct
-//     let api = Kucoin::new(KucoinEnv::Live, Some(credentials))?;
+    // Initialize the Kucoin API struct
+    let api = Kucoin::new(KucoinEnv::Live, Some(credentials))?;
      
-//     // Generate the dynamic Public or Private websocket url and endpoint from Kucoin
-//     let url = api.get_socket_endpoint(WSType::Public).await?;
+    // Generate the dynamic Public or Private websocket url and endpoint from Kucoin
+    let url = api.get_socket_endpoint(WSType::Public).await?;
      
-//     // Initialize the websocket
-//     let mut ws = api.websocket();
+    // Initialize the websocket
+    let mut ws = api.websocket();
 
-//     // Generate a Vec<WSTopic> of desired subs.They need to be public or private depending on the url
-//     let subs = vec![WSTopic::OrderBook(vec!["BTC-USDT".to_string()])];
+    // Generate a Vec<WSTopic> of desired subs.They need to be public or private depending on the url
+    let subs = vec![WSTopic::OrderBook(vec!["BTC-USDT".to_string()])];
 
-//     // Initalize your subscription and use await to unwrap the future   
-//     ws.subscribe(url, subs).await?;    
+    // Initalize your subscription and use await to unwrap the future   
+    ws.subscribe(url, subs).await?;    
 
-//     // Handle incoming responses matching messages
-//     while let Some(msg) = ws.try_next().await? {
-//         match msg {
-//             KucoinWebsocketMsg::OrderBookMsg(msg) => println!("{:#?}", msg),
-//             KucoinWebsocketMsg::PongMsg(msg) => println!("{:#?}", msg),     // Optional
-//             KucoinWebsocketMsg::WelcomeMsg(msg) => println!("{:#?}", msg),  // Optional
-//             _ => (),
-//         }
-//     }
-//     Ok(())
-// }
+    // Handle incoming responses matching messages
+    while let Some(msg) = ws.try_next().await? {
+        match msg {
+            KucoinWebsocketMsg::OrderBookMsg(msg) => println!("{:#?}", msg),
+            KucoinWebsocketMsg::PongMsg(msg) => println!("{:#?}", msg),     // Optional
+            KucoinWebsocketMsg::WelcomeMsg(msg) => println!("{:#?}", msg),  // Optional
+            _ => (),
+        }
+    }
+    Ok(())
+}
 
 #[tokio::main]
 pub async fn init_sandbox_api() -> Result<(), Box<dyn Error>> {
