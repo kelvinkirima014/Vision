be21965a8a427b77874d65caab28e44a2860b656
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-12-16 17:23:24 +0300

    remove comments in api_credentials.rs

--- src/api_credentials.rs
+++ src/api_credentials.rs
@@ -13,8 +13,6 @@ pub struct ApiCredentials {
 }
 
 pub fn load_api_credentials() -> ApiCredentials {
-    //let file_str = fs::read_to_string("./credentials.json").expect("No settings.json file found!");
-    //let deserialize_creds = serde_json::from_str::<ApiCredentials>(&SETTINGS_JSON.to_string()).expect("Unable to deserialize credentials.json!");
     let mut deserialize_creds = serde_json::from_str::<Vec<ApiCredentials>>(&CREDENTIALS_JSON).expect("Error deserializing JSON data");
     deserialize_creds.swap_remove(0)
     
