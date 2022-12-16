2171f33c6b05419230e1f03767a9e98ba0df14e2
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-12-16 17:46:53 +0300

    update sandbox api

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ fn main() {
     let creds = api_credentials::load_api_credentials();
     let api_s = api::init_kucoin_api(creds);
     println!("{:?}", api_s);
-    // let sandbox_api = api::init_sandbox_api();
-    // println!("{:?}", sandbox_api);
+
+    let sandbox_creds = api_credentials::load_api_credentials();
+    let sandbox_api = api::init_sandbox_api(sandbox_creds);
+    println!("{:?}", sandbox_api);
 }
