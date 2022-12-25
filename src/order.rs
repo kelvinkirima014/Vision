568fb929ce61cbf7008650dd099779435498f907
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-12-25 17:52:39 +0300

    add user info

--- src/order.rs
+++ src/order.rs
@@ -1,8 +1,10 @@
 use kucoin_rs::kucoin::{ user, websocket };
+use kucoin_rs::kucoin::client::Kucoin;
 use std::error::Error;
 
 
-pub async fn get_order_pairs() -> Result<(), Box<dyn Error>> {
-     let user_info = user::get_user_subaccount_info();
+pub async fn get_order_pairs(user_i: Kucoin) -> Result<(), Box<dyn Error>> {
+    
+     let user_info = user_i.get_user_subaccount_info();
     Ok(())
 }
